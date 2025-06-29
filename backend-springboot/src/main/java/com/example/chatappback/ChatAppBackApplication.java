5d1267a75bec2075f7c07deeec3acea4e0a4d291
zakki-z <159003869+zakki-z@users.noreply.github.com>
2025-06-29 01:29:20 +0100

    commit

---
+++ backend-springboot/src/main/java/com/example/chatappback/ChatAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppBackApplication.class, args);
+    }
+
+}
---
+++ backend-springboot/src/test/java/com/example/chatappback/ChatAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
