0f7f54252491dfe36acfcd55f8d511dd1e3071b3
ayoubaissaoui <ayoubaissaoui19.03@gmail.com>
2025-07-13 15:20:22 +0100

    all

---
+++ backend-springboot/chatappSpringboot/src/test/java/com/chat/chatapp/ChatappApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chat.chatapp;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApplicationTests {
+    @Disabled
+    @Test
+    void contextLoads() {
+    }
+
+}
