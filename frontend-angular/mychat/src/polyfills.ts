b0e94acb52e1b8712122de627848d2981e574866
AyoubAissaoui <ayoubaissaoui19.03@gmail.com>
2025-07-14 17:27:02 +0100

    angular

---
+++ frontend-angular/mychat/src/polyfills.ts
@@ -0,0 +1,13 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
