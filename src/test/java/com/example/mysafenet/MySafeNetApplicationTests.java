57bb88431a68446293a36d9b5b19248ca40b3b1b
zaki azhari <zakariaeazhari@proton.me>
2025-09-18 03:15:19 +0100

    intial commit

---
+++ src/test/java/com/example/mysafenet/MySafeNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysafenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySafeNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
