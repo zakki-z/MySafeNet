552226a42f02364485504dd4a7adb342e196f6e4
zaki azhari <zakariaeazhari@proton.me>
2025-10-18 23:27:40 +0100

    remove security for development

--- code/backend/src/main/java/com/example/mysafenet/entity/user/User.java
+++ code/backend/src/main/java/com/example/mysafenet/entity/user/User.java
@@ -5,12 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -18,7 +13,7 @@ import java.util.UUID;
 @DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
 @Data
 @Table(name = "users")
-public abstract class User implements UserDetails {
+public abstract class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -54,41 +49,5 @@ public abstract class User implements UserDetails {
     @Column(nullable = false)
     private String status;
 
-    // Spring Security UserDetails implementation
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // Return role based on discriminator value
-        String role = this.getClass().getAnnotation(DiscriminatorValue.class).value();
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
-    }
 
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return "ACTIVE".equalsIgnoreCase(status);
-    }
 }
